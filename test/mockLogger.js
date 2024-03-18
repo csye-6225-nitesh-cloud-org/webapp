8788f35baa9f4d82515a685ea9046a6111c69bb5
nitesh <more.ni@northeastern.edu>
2024-03-17 20:49:10 -0400

    code fix for integration test

---
+++ test/mockLogger.js
@@ -0,0 +1,8 @@
+const logger = {
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+    debug: jest.fn(),
+};
+
+module.exports = logger;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+jest.mock('../config/logger', () => require('../test/mockLogger'));
 const request = require('supertest');
 const app = require('../app');
 const User = require("../models/user.model");
